8ecead1e65924c61357b9b0a4ccb35d85e811f91
Sridharvn <sridharvnampoothiripad@gmail.com>
2022-11-12 17:10:41 +0530

    Homepage styled

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,32 @@
-import { StyleSheet, Text, View,Image } from 'react-native'
+import { StyleSheet, Text, View,Image,SafeAreaView } from 'react-native'
 import React from 'react'
 
 const Home = () => {
   return (
-    <View>
-          <Image source={require('./../../assets/images/HomeImage.png')} />
-          <Text>Welcome</Text>
-    </View>
-  )
+    <SafeAreaView style={styles.container}>
+      <Image
+        source={require("./../../assets/images/HomeImage.png")}
+        style={styles.image}
+      />
+      <Text style={styles.welcome}>Welcome</Text>
+    </SafeAreaView>
+  );
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+  },
+  welcome: {
+    fontSize: 40,
+    textAlign: "center",
+    margin: 100,
+    color: "#3338c7",
+    fontWeight: "bold",
+  },
+});
